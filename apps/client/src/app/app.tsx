279c230bdbe135e97632af99e111f251ddc7fb55
Jonny Björnhager <jonny.bjornhager@live.se>
2024-09-09 16:59:26 +0200

    add server app

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="client" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
